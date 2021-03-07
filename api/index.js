5ea968f2e5160bf0cffb204fa1bc9e1a98b9814b
Hans362 <i@hans362.cn>
2021-03-07 12:35:30 +0800

    Add Github Actions config

---
+++ api/index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+const gtmetrix = require('gtmetrix')({
+    email: process.env.EMAIL,
+    apikey: process.env.APIKEY,
+    timeout: 10000
+});
+
+const testDetails = {
+    url: process.env.URL,
+    location: 1,
+    browser: 3
+};
+
+gtmetrix.test.create(testDetails).then(data =>
+    gtmetrix.test.get(data.test_id, 5000).then(data =>
+        fs.writeFile('ct.json', JSON.stringify(data.append()), console.log)));
